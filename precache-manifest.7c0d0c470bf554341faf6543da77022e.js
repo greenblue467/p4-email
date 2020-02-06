5dc82846a609bf5606710c598e5cbda96440d802
greenblue467 <greenblue467@hotmail.com>
2020-02-06 20:47:34 +0800

    Updates

---
+++ precache-manifest.7c0d0c470bf554341faf6543da77022e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56d67100a6a1e288894c863ef1b2e81d",
+    "url": "/p4-email/index.html"
+  },
+  {
+    "revision": "9c13eaf2eacb098deb72",
+    "url": "/p4-email/static/css/main.258d240a.chunk.css"
+  },
+  {
+    "revision": "1fa235e548056089f94c",
+    "url": "/p4-email/static/js/2.03897f64.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/p4-email/static/js/2.03897f64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c13eaf2eacb098deb72",
+    "url": "/p4-email/static/js/main.8b743ee4.chunk.js"
+  },
+  {
+    "revision": "7f9c399175b98105e207",
+    "url": "/p4-email/static/js/runtime-main.e3b9209a.js"
+  }
+]);
